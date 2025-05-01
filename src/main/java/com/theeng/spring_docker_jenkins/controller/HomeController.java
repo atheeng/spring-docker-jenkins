b62398f7bec95638a0984aca7faa5ff36da854bd
Arun Tamang <aruntamang365@gmail.com>
2025-05-01 19:33:13 +0900

    Initial commit

---
+++ src/main/java/com/theeng/spring_docker_jenkins/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.theeng.spring_docker_jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "welcome to spring boot project";
+    }
+}
---
+++ src/test/java/com/theeng/spring_docker_jenkins/SpringDockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theeng.spring_docker_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
